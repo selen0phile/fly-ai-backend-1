a2f5f9715597771c7dc5ec03245a11f018f8dbb0
Anik Saha <aaniksahaa.2001@gmail.com>
2023-07-14 02:53:13 +0600

    sfsf

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -2,6 +2,7 @@ const errorhandler = (err, req, res, next) => {
     console.log("middleware/errorhandler.js");
     const errStatus = err.statusCode || 500;
     const errMsg = err.message || 'Something went wrong';
+    console.log(errMsg)
     res.status(errStatus).json({
         success: false,
         status: errStatus,
